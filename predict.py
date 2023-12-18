53c6dd3d9d2596a8b0df0ae7dd27102b4a710e4a
gatiambaliya04 <142475225+gatiambaliya04@users.noreply.github.com>
2023-12-18 20:39:37 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,28 +1,62 @@
-# Assume this is your predict.py file content
-# Save this content in a file named "predict.py"
+import argparse
+import torch
+from torchvision import models, transforms
+from PIL import Image
+import json
 
-import tensorflow as tf
-from tensorflow.keras.models import load_model
-import matplotlib.pyplot as plt
+parser = argparse.ArgumentParser(description='Predict the flower class of an image')
+parser.add_argument('--image_path', type=str, default='path/to/your/image.jpg', help='Path to the input image')
+parser.add_argument('--checkpoint', type=str, default='flower_classification_checkpoint.pth', help='Path to the model checkpoint')
+parser.add_argument('--top_k', type=int, default=5, help='Top K most probable classes')
+parser.add_argument('--category_names', type=str, default='cat_to_name.json', help='Path to category names mapping file')
+parser.add_argument('--gpu', action='store_true', help='Use GPU for prediction')
 
-# Load the trained model
-model = load_model("image_classification_model.h5")
+args = parser.parse_args()
 
-# Load and preprocess a sample image (you can replace this with your own image)
-sample_image_path = "path/to/your/sample/image.jpg"
-sample_image = plt.imread(sample_image_path)
-sample_image = tf.image.resize(sample_image, (32, 32))
-sample_image = sample_image / 255.0
-sample_image = tf.expand_dims(sample_image, 0)  # Add batch dimension
+# Load the model checkpoint
+checkpoint = torch.load(args.checkpoint)
+arch = checkpoint['arch']
 
-# Make predictions
-predictions = model.predict(sample_image)
+# Load the pre-trained model
+if arch == 'vgg16':
+    model = models.vgg16(pretrained=True)
+    input_size = 25088
+elif arch == 'resnet18':
+    model = models.resnet18(pretrained=True)
+    input_size = 512
 
-# Display the prediction
-predicted_class_index = tf.argmax(predictions[0])
-predicted_class = class_names[predicted_class_index]
-confidence = tf.reduce_max(tf.nn.softmax(predictions[0])) * 100
+# Replace the model classifier
+model.classifier = checkpoint['classifier']
+model.load_state_dict(checkpoint['state_dict'])
+model.class_to_idx = checkpoint['class_to_idx']
 
-plt.imshow(sample_image[0])
-plt.title(f"Predicted Class: {predicted_class}\nConfidence: {confidence:.2f}%")
-plt.show()
+# Move model to device
+device = torch.device("cuda" if args.gpu and torch.cuda.is_available() else "cpu")
+model.to(device)
+
+# Load and preprocess the input image
+image = Image.open(args.image_path)
+image = data_transforms['val'](image)
+image = image.unsqueeze(0).to(device)
+
+# Make prediction
+model.eval()
+with torch.no_grad():
+    output = model(image)
+
+# Get top K probabilities and class indices
+probs, indices = torch.topk(torch.exp(output), args.top_k)
+probs = probs.cpu().numpy()[0]
+indices = indices.cpu().numpy()[0]
+
+# Load category names mapping
+with open(args.category_names, 'r') as f:
+    cat_to_name = json.load(f)
+
+# Map indices to class names
+class_names = [cat_to_name[str(model.class_to_idx[str(idx)])] for idx in indices]
+
+# Print the results
+print("\nTop K Classes and Probabilities:")
+for i in range(args.top_k):
+    print(f"{class_names[i]}: {probs[i]*100:.2f}%")
